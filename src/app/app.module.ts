c134bd8a6b5b4944e8e1187a45ae19f09da3026c
suprajaarthi <supr17318.cs@rmke.ac.in>
2021-02-17 19:44:14 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PostsAddComponent } from './posts-add/posts-add.component';
+
+
+const appRoutes : Routes =[
+{path:'hero',component:HomeComponent},
+{path:'',component:AppComponent},
+{path:'posts-add',component:PostsAddComponent},
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostsAddComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule, 
+    RouterModule.forRoot(appRoutes)
    
   ],
   providers: [],
